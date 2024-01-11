22992dd622e31afeddf778aad84359605dfd8b42
timabs <timur.abd.nyc@gmail.com>
2024-01-11 17:44:33 -0500

    Cookie adjustments

--- middleware/userID.js
+++ middleware/userID.js
@@ -4,8 +4,9 @@ const uIDMiddleware = (req, res, next) => {
   const userId = req.cookies.userId || generateNewUserId();
   res.cookie("userId", userId, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === "production",
-    httpOnly: true,
+    // secure: process.env.NODE_ENV === "production",
+    httpOnly: false,
+    sameSite: "none",
   }); // Cookie lasts for 30 days
   req.userId = userId;
   next();
