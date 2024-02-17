7a3e0b4dacca4215408473bf82adbe46fe588a0d
timabs <timur.abd.nyc@gmail.com>
2024-02-17 09:54:17 -0500

    Test add to whitelist

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const contacts = require("./routes/contactsRouter");
 const images = require("./routes/imagesRouter");
 const form = require("./routes/contactFormRouter");
 const cors = require("cors");
-const whitelist = ["https://fancreate.netlify.app"];
+const whitelist = ["https://fancreate.netlify.app", "http://localhost:3000"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
